b35449dde29e811831c52930ff1acfa68cb917a7
iBristlecone <45261950+heyiWF@users.noreply.github.com>
2024-11-11 12:55:36 +0000

    调整二值化阈值

--- mos_detect.py
+++ mos_detect.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 import sys
+import cv2
 
 from EDAPublic.EDACV import CV_RIGHT, CV_LEFT, CV_UP, CV_DOWN
 
@@ -216,8 +217,8 @@ def detect_mos(image_path):
     # sort the peaks
     column_peaks = np.sort(column_peaks)
     row_peaks = np.sort(row_peaks)
-    second_peak_index_column, relative_position_column = find_second_peak(column_peaks, column_black_ratios)
-    second_peak_index_row, relative_position_row = find_second_peak(row_peaks, row_black_ratios)
+    # second_peak_index_column, relative_position_column = find_second_peak(column_peaks, column_black_ratios)
+    # second_peak_index_row, relative_position_row = find_second_peak(row_peaks, row_black_ratios)
 
     # save_path = image_path.replace('.png', '_histogram.png')  # 保存图片的路径
     # plot_ratios(image_path, save_path, column_black_ratios, column_white_ratios, row_black_ratios, row_white_ratios, column_peaks, row_peaks)
@@ -240,8 +241,8 @@ def detect_mos(image_path):
     height, width = binary_array.shape
     center_column = width // 2
     center_row = height // 2
-    distance_column = abs(second_peak_index_column - center_column)
-    distance_row = abs(second_peak_index_row - center_row)
+    distance_column = abs(column_second_peak - center_column)
+    distance_row = abs(row_second_peak - center_row)
     # print(f'Distance to center (column): {distance_column}')
     # print(f'Distance to center (row): {distance_row}')
     if distance_column > distance_row:
@@ -268,7 +269,7 @@ def detect_mos(image_path):
             result['Gate'] = CV_DOWN
 
     result['Body'] = result['Source']
-    # print(result)
+    print(result)
 
     # save_path = image_path.replace('.png', '_annotated.png')  # 保存图片的路径
     # annotate_image(image_path, save_path, result)
--- my_solution.py
+++ my_solution.py
@@ -1,10 +1,9 @@
 import os.path
 import sys
+import time
 
 from EDADataSet.EDADataset import EDADataset
 from EDAModel.NetlistModel import NetlistModel
-
-run_index = 0
 
 def run_main(data_dir: str = './image.png'):
     tmp_dir = './tmp/'
@@ -18,13 +17,43 @@ def run_main(data_dir: str = './image.png'):
         print(f"开始转换{os.path.basename(file_path)}")
         netlist = NetlistModel()
         output_netlist = netlist.run(file_path, os.path.basename(file_path), graph, info, tmp_dir, is_draw=False)
-        with open(os.path.basename(file_path).replace("png","txt"), "w") as file:
+        with open("generate/" + os.path.basename(file_path).replace("png","txt"), "w") as file:
             file.write(str(output_netlist))
         print(f"完成转换{os.path.basename(file_path)}")
 
+def transform(data_dir: str = './image.png', tag: str = '.'):
+    tmp_dir = './tmp/'
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    dataset = EDADataset(data_dir, need_read_json=False)
+    if tag is not None:
+        print(f"开始转换`{tag}`: ", end="")
+        start_time = time.time()
+    for file_index in range(len(dataset)):
+        t1 = time.time() if tag is not None else 0
+        graph, info, file_path = dataset[file_index]
+        netlist = NetlistModel()
+        output_netlist = netlist.run(file_path, os.path.basename(file_path), graph, info, tmp_dir, is_draw=False)
+        with open(os.path.basename(file_path).replace("png","txt"), "w") as file:
+            file.write(str(output_netlist))
+        t2 = time.time() if tag is not None else 0
+        if tag is not None:
+            average_time = (t2 - start_time) / (file_index + 1)
+            pass_time = t2 - start_time
+            pred_time = (len(dataset) - file_index - 1) * average_time
+            # 转换成时分秒
+            pass_time_str = time.strftime("%H:%M:%S", time.gmtime(pass_time))
+            pred_time_str = time.strftime("%H:%M:%S", time.gmtime(pred_time))
+            print(f"\r转换`{tag}` 中: {file_index:4d}/{len(dataset):4d}[{file_index/ len(dataset) * 100 :.2f}%] [{int((t2-t1)*1000):3d}ms/it] {pass_time_str} < {pred_time_str}", end="")
+    if tag is not None:
+        end_time = time.time()
+        all_time_str = time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))
+        print(f"\r转换`{tag}`完成: [{all_time_str}]")
+    
 
 if __name__ == "__main__":
     if sys.argv.__len__() == 2:
-        run_main(sys.argv[1])
+        transform(sys.argv[1])
     else:
-        run_main()
+        transform()
